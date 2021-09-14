e8c0dd5ae39663399af348d1cffcf08f4887cac4
Viljami Hirvonen <viljami.hirvonen@geniem.com>
2021-09-14 13:52:48 +0300

    Handle archive breadcrumbs

--- lib/PostType/Artist.php
+++ lib/PostType/Artist.php
@@ -5,6 +5,7 @@
 
 namespace TMS\Theme\Filharmonia\PostType;
 
+use Closure;
 use TMS\Theme\Base\Interfaces\PostType;
 
 /**
@@ -62,12 +63,16 @@ class Artist implements PostType {
      * @return void
      */
     public function hooks() : void {
-        add_action( 'init', \Closure::fromCallable( [ $this, 'register' ] ), 15 );
+        add_action( 'init', Closure::fromCallable( [ $this, 'register' ] ), 15 );
         add_filter(
             'tms/base/breadcrumbs/before_prepare',
-            \Closure::fromCallable( [ $this, 'format_single_breadcrumbs' ] ),
+            Closure::fromCallable( [ $this, 'format_single_breadcrumbs' ] ),
             10,
             3
+        );
+        add_filter(
+            'tms/base/breadcrumbs/after_prepare',
+            Closure::fromCallable( [ $this, 'format_archive_breadcrumbs' ] ),
         );
     }
 
@@ -148,6 +153,29 @@ class Artist implements PostType {
     }
 
     /**
+     * Get archive breadcrumbs base.
+     *
+     * @param false $is_cpt_archive Defines if cpt archive link is active.
+     *
+     * @return array[]
+     */
+    private function get_breadcrumbs_base( $is_cpt_archive = false ) : array {
+        return [
+            'home' => [
+                'title'     => _x( 'Home', 'Breadcrumbs', 'tms-theme-filharmonia' ),
+                'permalink' => trailingslashit( get_home_url() ),
+                'icon'      => '',
+            ],
+            [
+                'title'     => _x( 'Orchestra', 'Breadcrumb text', 'tms-theme-filharmonia' ),
+                'permalink' => get_post_type_archive_link( self::SLUG ),
+                'icon'      => false,
+                'is_active' => $is_cpt_archive,
+            ],
+        ];
+    }
+
+    /**
      * Format single view breadcrumbs.
      *
      * @param array  $breadcrumbs  Default breadcrumbs.
@@ -161,24 +189,30 @@ class Artist implements PostType {
             return $breadcrumbs;
         }
 
-        return [
-            'home' => [
-                'title'     => _x( 'Home', 'Breadcrumbs', 'tms-theme-base' ),
-                'permalink' => trailingslashit( get_home_url() ),
-                'icon'      => '',
-            ],
-            [
-                'title'     => _x( 'Orchestra', 'Breadcrumb text', 'tms-theme-base' ),
-                'permalink' => get_post_type_archive_link( self::SLUG ),
-                'icon'      => false,
-            ],
-            [
-                'title'     => get_the_title( $current_id ),
-                'permalink' => false,
-                'icon'      => false,
-                'is_active' => true,
-            ],
+        $breadcrumbs   = $this->get_breadcrumbs_base();
+        $breadcrumbs[] = [
+            'title'     => get_the_title( $current_id ),
+            'permalink' => false,
+            'icon'      => false,
+            'is_active' => true,
         ];
+
+        return $breadcrumbs;
+    }
+
+    /**
+     * Format archive view breadcrumbs.
+     *
+     * @param array $breadcrumbs Default breadcrumbs.
+     *
+     * @return array[]
+     */
+    public function format_archive_breadcrumbs( $breadcrumbs ) {
+        if ( ! is_post_type_archive( self::SLUG ) ) {
+            return $breadcrumbs;
+        }
+
+        return $this->get_breadcrumbs_base( true );
     }
 
     /**
