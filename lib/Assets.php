f5d8350601b0a81ecd498fb5a7f4ad023aa18650
Sami Harju <sami.harju@geniem.com>
2021-09-30 14:54:39 +0300

    Filtered tms/theme/admin_js_path

--- lib/Assets.php
+++ lib/Assets.php
@@ -22,6 +22,7 @@ class Assets extends \TMS\Theme\Base\Assets implements \TMS\Theme\Base\Interface
         add_filter( 'tms/theme/theme_selected', [ $this, 'theme_name' ] );
 
         add_filter( 'tms/theme/theme_css_path', [ $this, 'theme_asset_path' ], 10, 2 );
+        add_filter( 'tms/theme/admin_js_path', [ $this, 'theme_asset_path' ], 10, 2 );
 
         add_filter( 'tms/theme/asset_mod_time', function ( $mod_time, $filename ) {
             if ( false !== strpos( $filename, 'filharmonia' ) ) {
